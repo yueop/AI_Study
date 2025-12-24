8b67c1d64edf9acd46e3f95c1418efb95b3c0ffb
이유업 <eunheep16@gmail.com>
2025-12-24 14:45:38 +0900

    넘파이 개념 정리

---
+++ python/arrOperatorEx1.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+arr1 = np.arange(1, 5)
+arr2 = np.ones(4)
+
+print(arr1 , arr2)
+print(arr1 + arr2)
+print(arr1 - arr2)
+print(arr1 > arr2)
---
+++ python/arrOperatorEx2.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+arr1 = np.random.randint(11, size=4)
+print(arr1)
+print(arr1 + 2)
+print(arr1 * 2)
